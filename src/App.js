9ef6d5c9ba8cfb2cd41468753f54c8b8cbe528de
legna <legnajaneth@hotmail.com>
2024-11-08 20:38:41 -0800

    deployment issue fix

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
     <main>
-      <Router>
+      <Router basename="/JNS_AUTO">
         <NavigationBar/>
           <Routes>
             <Route path="/" element={<HomePage />} />
